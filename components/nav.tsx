92acc1a770f0346ea035ce7d2fb131d294041475
sidhant2152 <sidhantkapoor085@gmail.com>
2025-12-25 04:37:00 +0530

    feat: initial commit

---
+++ components/nav.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div className='px-4 py-6 border-b border-gray-500'>
+      <h1 className='text-4xl font-bold'>
+        Finboard Assignment
+      </h1>
+    </div>
+  )
+}
